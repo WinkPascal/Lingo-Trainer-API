f99840078e315760ccb979d0f966847421cfc561
WinkPascal <46743303+WinkPascal@users.noreply.github.com>
2020-11-22 22:59:47 +0100

    continious intergration fix

--- src/main/java/nl/hu/lingo/Controllers/WordController.java
+++ src/main/java/nl/hu/lingo/Controllers/WordController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WordController {
 @GetMapping("/hello")
     public String getHelloWorld(){
-        return "hello world";
+        return "hello wosrld";
     }
 }
