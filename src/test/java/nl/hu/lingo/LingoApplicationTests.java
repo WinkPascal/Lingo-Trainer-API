b169d7298cc942d2f10efe97da8610c3ae8f7ed5
WinkPascal <46743303+WinkPascal@users.noreply.github.com>
2020-11-25 15:17:20 +0100

    prerun script gemaakt

--- src/test/java/nl/hu/lingo/LingoApplicationTests.java
+++ src/test/java/nl/hu/lingo/LingoApplicationTests.java
@@ -1,5 +1,6 @@
 package nl.hu.lingo;
 
+import nl.hu.lingo.Domain.Game;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,8 @@ class LingoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Game game = new Game();
+		game.filterWords(7);
 	}
 
 }
